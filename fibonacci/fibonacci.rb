91a3f5b8b617c08166d8977ebbafa3f24b782961
Adam Cohn <adamcohn88@gmail.com>
2018-05-23 10:06:22 -0400

    done

--- fibonacci/fibonacci.rb
+++ fibonacci/fibonacci.rb
@@ -1,11 +1,11 @@
 def fibonacci(n, num_one=0, num_two=1, count=0)
-  return  num_one if count == n
-  num_one = num_two
-  num_two = num_one + num_two
+  return  puts num_one if count == n
+  new_num_one = num_two
+  new_num_two = num_one + num_two
   count += 1
-  fibonacci(n, num_one, num_two, count)
+  fibonacci(n, new_num_one, new_num_two, count)
 end
 
-fibonacci(2)
+fibonacci(8)
 
 
